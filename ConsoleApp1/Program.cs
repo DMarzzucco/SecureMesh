0ce38e59692249fcb6dcd323dcb1d06b01bed5fd
Dmarzzucco <ksksks>
2024-05-08 15:44:50 -0300

    fresh

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,16 +2,46 @@
 
 namespace MiProyecto
 {
-    /*class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Tu esposo es un pajero");
-        }
-    }*/
     class Program
     {
         static void Main()
+        {
+            //TypeClass.TypVar();
+            //PrintTerminal.Print();
+            Edad.MainEdad();
+        }
+    }
+    class Edad
+    {
+        public static void MainEdad()
+        {
+            int edad = 8;
+            bool esMayor = EsMayorDeEdad(edad);
+
+            if (esMayor)
+            {
+                Console.WriteLine("la persona es mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("Es Menor");
+            }
+        }
+        public static bool EsMayorDeEdad(int edad)
+        {
+            if (edad >= 10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    class TypeClass
+    {
+        public static void TypVar()
         {
             //vars/lets
             int NumeroEntero;
@@ -22,10 +52,17 @@ namespace MiProyecto
             NumeroEntero = 23;
             //imprimir valores de las variables
 
-            Console.WriteLine("int :"+ NumeroEntero);
+            Console.WriteLine("int :" + NumeroEntero);
             Console.WriteLine("double:" + numberDecimal);
             Console.WriteLine("string: " + cadenaTexto);
-            Console.WriteLine("boolean:"+ booleano);
+            Console.WriteLine("boolean:" + booleano);
+        }
+    }
+    class PrintTerminal
+    {
+        public static void Print()
+        {
+            Console.WriteLine("Holaaa");
         }
     }
 }
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2bac7079295bf6000ef98b3f456718787ea0d31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4901cf5c7c4ff390b436d005277c853534ee35e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
