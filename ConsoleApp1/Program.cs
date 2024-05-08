4901cf5c7c4ff390b436d005277c853534ee35e4
Dmarzzucco <ksksks>
2024-05-08 12:40:49 -0300

    first

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MiProyecto
+{
+    /*class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tu esposo es un pajero");
+        }
+    }*/
+    class Program
+    {
+        static void Main()
+        {
+            //vars/lets
+            int NumeroEntero;
+            double numberDecimal = 1.2;
+            string cadenaTexto = "Texto de prueba";
+            bool booleano = true;
+            //asignar valores
+            NumeroEntero = 23;
+            //imprimir valores de las variables
+
+            Console.WriteLine("int :"+ NumeroEntero);
+            Console.WriteLine("double:" + numberDecimal);
+            Console.WriteLine("string: " + cadenaTexto);
+            Console.WriteLine("boolean:"+ booleano);
+        }
+    }
+}
